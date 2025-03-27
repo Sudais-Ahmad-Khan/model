6860e218329cab776da87f09cada8123c6218e3a
Sudais-Ahmad-Khan <chochona12345@gmail.com>
2025-03-27 12:06:01 +0500

    Update model.js

--- model.js
+++ model.js
@@ -1,8 +1,8 @@
 let allCards = document.querySelectorAll('.card');
 let outerModel = document.querySelector('.model__outer');
 let outerInner = document.querySelector('.model__inner');
-let close = document.querySelector('svg');
 let description = document.querySelector('.description').innerHTML;
+let closeIcon = document.querySelector('svg');
 
 allCards.forEach(function(card){
     card.addEventListener("click", function(e){
@@ -19,25 +19,26 @@ allCards.forEach(function(card){
         outerInner.insertAdjacentHTML('afterbegin', html);
     });
 });
-outerModel.addEventListener("click",function(e){
-    outerInner.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>`;
+outerModel.addEventListener("click", function(e){
     if(!e.target.closest('.model__inner')){
+        outerInner.firstElementChild.nextElementSibling.remove();
+        outerInner.firstElementChild.remove();
         outerModel.classList.remove('open');
         outerModel.classList.remove('whenopen');
         outerInner.classList.remove('whenopen');
     }
 }); 
-close.addEventListener("click", function(){
-    outerInner.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>`;
-    if(!e.target.closest('.model__inner')){
+closeIcon.addEventListener("click", function(){
+        outerInner.firstElementChild.nextElementSibling.remove();
+        outerInner.firstElementChild.remove();
         outerModel.classList.remove('open');
         outerModel.classList.remove('whenopen');
         outerInner.classList.remove('whenopen');
-    }
 });
 window.addEventListener("keydown", function(e){
     if(e.key=='Escape'){
-        outerInner.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>`;
+        outerInner.firstElementChild.nextElementSibling.remove();
+        outerInner.firstElementChild.remove();
         outerModel.classList.remove('open');
         outerModel.classList.remove('whenopen');
         outerInner.classList.remove('whenopen');
